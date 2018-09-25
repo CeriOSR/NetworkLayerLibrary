aa7fea0abd298382d2ed6ae70043ce118bdff8a0
reycerio <ceriosrey@gmail.com>
2018-09-25 16:22:25 -0700

    Initial Commit

---
+++ NetworkLayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NetworkLayer
+//
+//  Created by Rey Cerio on 2018-09-25.
+//  Copyright © 2018 Rey Cerio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
